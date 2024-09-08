12c1fc9af34d3d670952ae5a8fd79a43a3e5f9af
AbhilashLenin.dev <118419822+iabhilashlenin@users.noreply.github.com>
2024-09-09 03:49:10 +0530

    Update UserContext.jsx

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -33,17 +33,18 @@ export function UserContextProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [id, setId] = useState(null);
 
-  useEffect(() => {
-    axios.get('/profile')
-      .then(response => {
-        setId(response.data.userId);
-        setUsername(response.data.username);
-      })
-      .catch(error => {
-        console.error('Failed to fetch user profile:', error);
-        // Handle the error case as needed (e.g., redirect to login page)
-      });
-  }, []);
+useEffect(() => {
+  axios.get('/profile', { withCredentials: true })
+    .then(response => {
+      setId(response.data.userId);
+      setUsername(response.data.username);
+    })
+    .catch(error => {
+      console.error('Failed to fetch user profile:', error);
+      // Handle the error case as needed (e.g., redirect to login page)
+    });
+}, []);
+
 
   return (
     <UserContext.Provider value={{ username, setUsername, id, setId }}>
